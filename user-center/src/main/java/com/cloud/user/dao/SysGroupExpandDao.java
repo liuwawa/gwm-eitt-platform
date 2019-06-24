f1f57bdef7d5336e21aad42b385319b9198d52d5
wangmaoyun <zhuidao4907099@163.com>
2019-06-24 10:50:23 +0800

    添加@mapper

--- user-center/src/main/java/com/cloud/user/dao/SysGroupExpandDao.java
+++ user-center/src/main/java/com/cloud/user/dao/SysGroupExpandDao.java
@@ -2,6 +2,8 @@ package com.cloud.user.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cloud.model.user.SysGroupExpand;
+import com.sun.tracing.dtrace.ModuleAttributes;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +13,7 @@ import com.cloud.model.user.SysGroupExpand;
  * @author liuek
  * @since 2019-06-21
  */
+@Mapper
 public interface SysGroupExpandDao extends BaseMapper<SysGroupExpand> {
 
 }
