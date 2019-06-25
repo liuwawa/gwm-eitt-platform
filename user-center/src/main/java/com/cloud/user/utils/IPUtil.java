7622897143d008d4b06345d895ce97cf64a26281
wangmaoyun <zhuidao4907099@163.com>
2019-06-25 10:52:18 +0800

    IPUtil 修改

--- user-center/src/main/java/com/cloud/user/utils/IPUtil.java
+++ user-center/src/main/java/com/cloud/user/utils/IPUtil.java
@@ -1,4 +1,4 @@
-package com.cloud.gateway.utils;
+package com.cloud.user.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -6,44 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-/**
- * Created by 王贺鹏 on 2018/7/10.
- */
 @Slf4j
 public class IPUtil {
-
-    public static String getIpAddress(HttpServletRequest request) {
-        // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
-
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-            }
-        } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (String ip1 : ips) {
-                String strIp = (String) ip1;
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
-        }
-        return ip;
-    }
 
     /**
      * 获取当前网络ip
@@ -80,7 +44,4 @@ public class IPUtil {
         }
         return ipAddress;
     }
-
-
-
 }
