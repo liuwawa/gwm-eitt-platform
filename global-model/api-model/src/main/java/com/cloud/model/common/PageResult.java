992c6f3dfc15a00aa8efe0b71a3a184308a552ad
WangPan <326676060@qq.com>
2019-07-03 13:53:30 +0800

    封装分页返回结果

---
+++ global-model/api-model/src/main/java/com/cloud/model/common/PageResult.java
@@ -0,0 +1,39 @@
+package com.cloud.model.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+/**
+ * 分页返回结果
+ * @author Louis
+ * @date Aug 19, 2018
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PageResult {
+	/**
+	 * 当前页码
+	 */
+	private long pageNum;
+	/**
+	 * 每页数量
+	 */
+	private long pageSize;
+	/**
+	 * 记录总数
+	 */
+	private long totalSize;
+	/**
+	 * 页码总数
+	 */
+	private long totalPages;
+	/**
+	 * 分页数据
+	 */
+	private List<?> content;
+}
