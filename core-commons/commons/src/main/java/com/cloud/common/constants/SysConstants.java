c211bea1d3d57bf2fbbe260e09a04811795948a5
WangPan <326676060@qq.com>
2019-07-04 08:53:03 +0800

    常量类

---
+++ core-commons/commons/src/main/java/com/cloud/common/constants/SysConstants.java
@@ -0,0 +1,15 @@
+package com.cloud.common.constants;
+
+/**
+ * 常量管理
+ * @author wp
+ * @date 2019.7.3
+ */ 
+public interface SysConstants {
+
+	/**
+	 * 系统管理员角色id
+	 */
+	Long ADMIN_ID = 1L;
+	
+}
