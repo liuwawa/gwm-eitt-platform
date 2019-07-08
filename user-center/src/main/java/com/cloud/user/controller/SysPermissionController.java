5602f0ab903b0f78a119f0815013ee369f88992a
WangPan <326676060@qq.com>
2019-07-08 11:07:04 +0800

    权限controller整合

--- user-center/src/main/java/com/cloud/user/controller/SysPermissionController.java
+++ user-center/src/main/java/com/cloud/user/controller/SysPermissionController.java
@@ -1,5 +1,24 @@
 package com.cloud.user.controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import com.cloud.common.utils.AppUserUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloud.model.common.Page;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.cloud.common.enums.ResponseStatus;
@@ -34,7 +53,29 @@ public class SysPermissionController {
     @LogAnnotation(module = LogModule.ADD_PERMISSION)
     @PreAuthorize("hasAuthority('back:permission:save')")
     @PostMapping("/permissions")
-    public ResultVo save(@RequestBody SysPermission sysPermission) {
+    public SysPermission save(@RequestBody SysPermission sysPermission) {
+        if (StringUtils.isBlank(sysPermission.getPermission())) {
+            throw new IllegalArgumentException("权限标识不能为空");
+        }
+        if (StringUtils.isBlank(sysPermission.getName())) {
+            throw new IllegalArgumentException("权限名不能为空");
+        }
+
+        sysPermissionService.save(sysPermission);
+
+        return sysPermission;
+    }
+
+    /**
+     * 管理后台添加权限(恩科 element ui)
+     *
+     * @param sysPermission
+     * @return
+     */
+    @LogAnnotation(module = LogModule.ADD_PERMISSION)
+    @PreAuthorize("hasAuthority('back:permission:save')")
+    @PostMapping("/permissions2")
+    public ResultVo save2(@RequestBody SysPermission sysPermission) {
         try {
             if (StringUtils.isBlank(sysPermission.getPermission())) {
                 return new ResultVo(500, "权限标识不能为空", null);
@@ -56,7 +97,6 @@ public class SysPermissionController {
             return new ResultVo(500, ResponseStatus.RESPONSE_OPERATION_ERROR.message, null);
         }
     }
-
     /**
      * 管理后台修改权限
      *
@@ -65,7 +105,25 @@ public class SysPermissionController {
     @LogAnnotation(module = LogModule.UPDATE_PERMISSION)
     @PreAuthorize("hasAuthority('back:permission:update')")
     @PutMapping("/permissions")
-    public ResultVo update(@RequestBody SysPermission sysPermission) {
+    public SysPermission update(@RequestBody SysPermission sysPermission) {
+        if (StringUtils.isBlank(sysPermission.getName())) {
+            throw new IllegalArgumentException("权限名不能为空");
+        }
+
+        sysPermissionService.update(sysPermission);
+
+		return sysPermission;
+	}
+
+    /**
+     * 管理后台修改权限（恩科 element ui）
+     *
+     * @param sysPermission
+     */
+    @LogAnnotation(module = LogModule.UPDATE_PERMISSION)
+    @PreAuthorize("hasAuthority('back:permission:update')")
+    @PutMapping("/permissions2")
+    public ResultVo update2(@RequestBody SysPermission sysPermission) {
         try {
             if (StringUtils.isBlank(sysPermission.getName())) {
                 throw new IllegalArgumentException("权限名不能为空");
@@ -93,7 +151,19 @@ public class SysPermissionController {
     @LogAnnotation(module = LogModule.DELETE_PERMISSION)
     @PreAuthorize("hasAuthority('back:permission:delete')")
     @DeleteMapping("/permissions/{id}")
-    public ResultVo delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id) {
+        sysPermissionService.delete(id);
+    }
+
+    /**
+     * 删除权限标识（恩科  element ui）
+     *
+     * @param id
+     */
+    @LogAnnotation(module = LogModule.DELETE_PERMISSION)
+    @PreAuthorize("hasAuthority('back:permission:delete')")
+    @DeleteMapping("/permissions2/{id}")
+    public ResultVo delete2(@PathVariable Long id) {
         try {
             sysPermissionService.delete(id);
             log.info("删除成功，权限id:{}", id);
@@ -109,8 +179,17 @@ public class SysPermissionController {
      * 查询所有的权限标识
      */
     @PreAuthorize("hasAuthority('back:permission:query')")
+    @GetMapping("/permissions")
+    public Page<SysPermission> findPermissions(@RequestParam Map<String, Object> params) {
+        return sysPermissionService.findPermissions(params);
+    }
+
+    /**
+     * 分页查询所有的权限标识分（恩科 element ui）
+     */
+    @PreAuthorize("hasAuthority('back:permission:query')")
     @PostMapping("/findPages")
-    public PageResult findPermissions(@RequestBody Map<String, Object> params) {
+    public PageResult findPermissionsPage(@RequestBody Map<String, Object> params) {
         Long pageIndex = Long.valueOf(params.get("pageNum").toString());
         Long pageSize = Long.valueOf(params.get("pageSize").toString());
         String name = String.valueOf(params.get("name").toString());
@@ -125,4 +204,41 @@ public class SysPermissionController {
                 totalPages(permissionIPage.getPages()).
                 totalSize(permissionIPage.getTotal()).build();
     }
+
+    /**
+     * 查询所有的权限标识(element ui)
+     */
+    @PreAuthorize("hasAuthority('back:permission:query')")
+    @PostMapping("/findAllPermissions")
+    public List<SysPermission> findAllPermissions() {
+        //		for (SysPermission sysPermission : list) {
+//		}
+//        Set<String> permissions = Objects.requireNonNull(AppUserUtil.getLoginAppUser()).getPermissions();
+//        List<SysPermission> list = sysPermissionService.list();
+//        list.forEach(l -> {
+//            if (permissions.contains(l.getPermission())) {
+//                l.setChecked(true);
+//            }
+//        });
+        return sysPermissionService.list();
+    }
+
+//    /**
+//     * 查询用户的权限标识(element ui)
+//     */
+//    @PreAuthorize("hasAuthority('back:permission:query')")
+//    @PostMapping("/findUserPermissions")
+//    public List<SysPermission> findUserPermissions() {
+////		List<SysPermission> list = sysPermissionService.list();
+////		for (SysPermission sysPermission : list) {
+////		}
+//        Set<String> permissions = Objects.requireNonNull(AppUserUtil.getLoginAppUser()).getPermissions();
+//        List<SysPermission> list = sysPermissionService.list();
+//        list.forEach(l -> {
+//            if (permissions.contains(l.getPermission())) {
+//                l.setChecked(true);
+//            }
+//        });
+//        return list;
+//    }
 }
