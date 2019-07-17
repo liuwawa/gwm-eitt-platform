ae40a8947ac941051327fed428070112fe4ababf
liangzheng <1365966466@qq.com>
2019-07-17 16:08:07 +0800

    单用户登陆 短信登陆

--- gateway-zuul/src/main/java/com/cloud/gateway/config/InterceptorConfig.java
+++ gateway-zuul/src/main/java/com/cloud/gateway/config/InterceptorConfig.java
@@ -1,14 +1,16 @@
 package com.cloud.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
-
+    @Autowired
+    private UserInterceptor userInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(userInterceptor).addPathPatterns("/**");
     }
 }
