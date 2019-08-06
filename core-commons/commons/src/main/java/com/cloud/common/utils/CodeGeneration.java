1419e9c105bf4a3ea005442d4eeb2ecad51289cc
wangmaoyun <zhuidao4907099@163.com>
2019-08-06 15:56:57 +0800

    员工自助mapper

--- core-commons/commons/src/main/java/com/cloud/common/utils/CodeGeneration.java
+++ core-commons/commons/src/main/java/com/cloud/common/utils/CodeGeneration.java
@@ -58,7 +58,7 @@ public class CodeGeneration {
         StrategyConfig strategy = new StrategyConfig();
         strategy.setTablePrefix(new String[]{"hr_"}); // 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel); // 表名生成策略
-        strategy.setInclude(new String[]{"hr_personnel"}); // 需要生成的表
+        strategy.setInclude(new String[]{"hr_repayment"}); // 需要生成的表
 
         strategy.setSuperServiceClass(null);
         strategy.setSuperServiceImplClass(null);
