be058de763f6026d8d66782fdeb9d9fa799bc40e
wangmaoyun <zhuidao4907099@163.com>
2019-08-06 17:33:45 +0800

    员工自助

--- personnel-center/src/main/java/com/cloud/personnel/controller/HrPersonnelController.java
+++ personnel-center/src/main/java/com/cloud/personnel/controller/HrPersonnelController.java
@@ -1,6 +1,10 @@
 package com.cloud.personnel.controller;
 
 
+import com.cloud.common.vo.ResultVo;
+import com.cloud.model.personnel.HrPersonnel;
+import com.cloud.personnel.util.PersonnelUtil;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/personnel")
 public class HrPersonnelController {
-
+    /**
+     * @return 查询结果
+     * 根据员工编号查询其员工履历
+     */
+    @GetMapping("/getPersonnelInfo")
+    public ResultVo findContractByPersonnelID() {
+        // 获取当前的登录对象个人信息
+        HrPersonnel hrPersonnel = PersonnelUtil.getPersonnel();
+        return new ResultVo(200, "操作成功", hrPersonnel);
+    }
 }
 
